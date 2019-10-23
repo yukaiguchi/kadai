38e83e9ba7dfd6e256fd07697101efb9d1873c8e
yukaiguchi <yurabi.071@gmail.com>
2019-10-23 05:16:05 +0000

    some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,13 @@
+const firstName = 'Yuka'
+const secondName = 'Iguchi'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+console.log(!hasDog && !hasCat)
